d22437139af6dd33c301d67b4b6c03ac59131fc2
sayyidalharis <101546647+sayyidalharis@users.noreply.github.com>
2023-08-17 13:33:08 +0700

    new

---
+++ tes.py
@@ -0,0 +1,17 @@
+# ask user to input a number
+# if the number is greater than 10, print "greater than 10"
+# if the number is less than 10, print "less than 10"
+# if the number is equal to 10, print "equal to 10"
+
+# ask user to input a number
+number = int(input("Enter a number: "))
+# if the number is greater than 10, print "greater than 10"
+if number > 10:
+    print("greater than 10")
+# if the number is less than 10, print "less than 10"
+elif number < 10:
+    print("less than 10")
+# if the number is equal to 10, print "equal to 10"
+else:
+    print("equal to 10")
+
